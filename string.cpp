df0b512f600b231daaa580a7fe9d2bd4fe104720
Daniel Rector <drector@vmware.com>
2022-06-04 18:18:21 -0400

    first commit

---
+++ string.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+#include <string>
+using std::string;
+
+int main()
+{
+    string name;
+    cout << "who are you? ";
+    cin >> name;
+    string greeting = "Hello, " + name;
+    if (name == "Daniel")
+    {
+        greeting += ", I know you!";
+    }
+    cout << greeting << '\n';
+
+    int l = greeting.length();
+    cout << "\"" + greeting + "\" is " << l << " characters long." << '\n';
+    string beginning = greeting.substr(greeting.find(' ') + 1);
+    cout << beginning << '\n';
+    if (beginning == name)
+    {
+        cout << "expected result." << '\n';
+    }
+
+    return 0;
+}
